dcfb99da1b9b5cfe5af1dcf14975c8cc32954475
Shashank K R <krshashank1118@gmail.com>
2024-03-23 23:04:54 +0530

    leetcode controller

--- controller/platform.controller.js
+++ controller/platform.controller.js
@@ -1,10 +1,47 @@
-import {ApiResponse, AsyncHandler} from "../utils/index.js";
+import {ApiError, ApiResponse, AsyncHandler} from "../utils/index.js";
+import axios from "axios";
+import cheerio from "cheerio";
+import fetch from 'node-fetch';
 
 const leetcodeController = AsyncHandler(async (req, res, next) => {
     const userId = req.params.userId;
-    // console.log(userId);
-    
-    res.json(new ApiResponse(200, "LeetCode Controller Working..."))
-})
+    console.log(userId);
+    if (!userId) {
+        throw new ApiError(400, "User Handle required");
+    }
 
-export {leetcodeController}
+    const graphqlQuery = `
+    {
+        userContestRanking(username: "${userId}") {
+            attendedContestsCount
+            rating
+            globalRanking
+            totalParticipants
+            topPercentage
+        }
+    }
+    `;
+
+    const response = await fetch('https://leetcode.com/graphql', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ query: graphqlQuery }),
+    });
+
+    if (!response.ok) {
+        throw new ApiError(404, "User not found on Leetcode");
+    }
+
+    const data = await response.json();
+    const finaldata = data.data.userContestRanking;
+    console.log(data.data.userContestRanking);
+
+    res.json(new ApiResponse(200, "Leetcode Controller Working...", finaldata));
+});
+
+
+export { 
+    leetcodeController ,
+};
