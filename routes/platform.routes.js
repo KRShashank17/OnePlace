a39b6cfa4bf1d5e852394b47cc7dcdacaa4d850f
Shashank K R <krshashank1118@gmail.com>
2024-03-26 23:12:25 +0530

    GFG Route added

--- routes/platform.routes.js
+++ routes/platform.routes.js
@@ -1,10 +1,11 @@
 import express from "express";
-import {codeChefController, codeForcesController, leetcodeController } from "../controller/platform.controller.js";
+import {codeChefController, codeForcesController, gfgController, leetcodeController } from "../controller/platform.controller.js";
 
 const router = express.Router();
 
 router.get("/leetcode/:userId" , leetcodeController);
 router.get("/codeforces/:userId" , codeForcesController);
 router.get("/codechef/:userId" , codeChefController);
+router.get("/gfg/:userId" , gfgController);
 
 export default router
